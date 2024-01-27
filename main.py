ad0af74704672c1322925dfcfc9e29f59fffac1a
brakcetin <brakcetin660@gmail.com>
2024-01-27 16:53:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#THIS IS A TUTORIAL PROJECT FROM https://youtu.be/l8Imtec4ReQ?si=R_qphKMilqJa9qBM
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+class TheLabApp(App):
+    pass
+
+TheLabApp().run()
